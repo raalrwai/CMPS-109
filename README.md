180d1514168330b94ef3646e144f3fc3349bbb19
Connor Thurmond <cthurmon@ucsc.edu>
2018-07-02 14:10:55 -0700

    Redoing starter code.

--- README.md
+++
@@ -1,2 +0,0 @@
-# asg1cmps109
-Assignment 1 for CMPS 109.
--- libfns.h
+++
@@ -1,8 +0,0 @@
-// $Id: libfns.h,v 1.2 2015-07-02 16:03:36-07 - - $
-
-// Library functions not members of any class.
-
-#include "bigint.h"
-
-bigint pow (const bigint& base, const bigint& exponent);
-
