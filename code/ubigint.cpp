6a04b3ff48172ad6bb48b2c6cecbc5e11e7d84cb
Rami Alrwais <raalrwai@unix3.lt.ucsc.edu>
2018-07-04 12:15:20 -0700

    message

--- code/ubigint.cpp
+++ code/ubigint.cpp
@@ -10,82 +10,88 @@ using namespace std;
 #include "ubigint.h"
 #include "debug.h"
 
-ubigint::ubigint (unsigned long that): uvalue (that) {
-   DEBUGF ('~', this << " -> " << ubig_value[0])
+ubigint::ubigint (unsigned long that)  {
+
+  stringstream ss;
+  string uvalue;
+
+  ss << that;
+  ss >> uvalue;
+  for(auto it = uvalue.begin(; it != uvalue.begin(); it++)){
+    ubig_value.push_back(*it);
+    DEBUGF('-', *it);
+  }
 }
 
 ubigint::ubigint (const string& that): uvalue(0) {
-   DEBUGF ('~', "that = \"" << that << "\"");
-   for (char digit: that) {
-      if (not isdigit (digit)) {
-         throw invalid_argument ("ubigint::ubigint(" + that + ")");
-      }
-      ubig_value = ubig_value[0] * 10 + digit - '0';
-   }
+  for (auto it = that.end(); it != that.begin(); i++){
+    ubig_value.push_back(*it):
+      DEBUGF('~', *it);
+  }
 }
 
 ubigint ubigint::operator+ (const ubigint& that) const {
-   return ubigint (ubig_value[0] + that.ubig_value[0]);
+  return ubigint (ubig_value[0] + that.ubig_value[0]);
 }
 
 ubigint ubigint::operator- (const ubigint& that) const {
-   if (*this < that) throw domain_error ("ubigint::operator-(a<b)");
-   return ubigint (ubig_value[0] - that.ubig_value[0]);
+  if (*this < that) throw domain_error ("ubigint::operator-(a<b)");
+  return ubigint (ubig_value[0] - that.ubig_value[0]);
 }
 
 ubigint ubigint::operator* (const ubigint& that) const {
-   return ubigint (ubig_value[0] * that.ubig_value[0]);
+  return ubigint (ubig_value[0] * that.ubig_value[0]);
 }
 
 void ubigint::multiply_by_2() {
-   ubig_value[0] *= 2;
+  ubig_value[0] *= 2;
 }
 
 void ubigint::divide_by_2() {
-   ubig_value[0] /= 2;
+  ubig_value[0] /= 2;
 }
 
 
 struct quo_rem { ubigint quotient; ubigint remainder; };
 quo_rem udivide (const ubigint& dividend, ubigint divisor) {
-   // Note: divisor is modified so pass by value (copy).
-   ubigint zero {0};
-   if (divisor == zero) throw domain_error ("udivide by zero");
-   ubigint power_of_2 {1};
-   ubigint quotient {0};
-   ubigint remainder {dividend}; // left operand, dividend
-   while (divisor < remainder) {
-      divisor.multiply_by_2();
-      power_of_2.multiply_by_2();
-   }
-   while (power_of_2 > zero) {
-      if (divisor <= remainder) {
-         remainder = remainder - divisor;
-         quotient = quotient + power_of_2;
-      }
-      divisor.divide_by_2();
-      power_of_2.divide_by_2();
-   }
-   return {.quotient = quotient, .remainder = remainder};
+  // Note: divisor is modified so pass by value (copy).
+  ubigint zero {0};
+  if (divisor == zero) throw domain_error ("udivide by zero");
+  ubigint power_of_2 {1};
+  ubigint quotient {0};
+  ubigint remainder {dividend}; // left operand, dividend
+  while (divisor < remainder) {
+    divisor.multiply_by_2();
+    power_of_2.multiply_by_2();
+  }
+  while (power_of_2 > zero) {
+    if (divisor <= remainder) {
+      remainder = remainder - divisor;
+      quotient = quotient + power_of_2;
+    }
+    divisor.divide_by_2();
+    power_of_2.divide_by_2();
+  }
+  return {.quotient = quotient, .remainder = remainder};
 }
 
 ubigint ubigint::operator/ (const ubigint& that) const {
-   return udivide (*this, that).quotient;
+  return udivide (*this, that).quotient;
 }
 
 ubigint ubigint::operator% (const ubigint& that) const {
-   return udivide (*this, that).remainder;
+  return udivide (*this, that).remainder;
 }
 
 bool ubigint::operator== (const ubigint& that) const {
-   return ubig_value[0] == that.ubig_value[0];
+  return ubig_value[0] == that.ubig_value[0];
 }
 
 bool ubigint::operator< (const ubigint& that) const {
-   return ubig_value[0] < that.ubig_value[0];
+  return ubig_value[0] < that.ubig_value[0];
 }
 
 ostream& operator<< (ostream& out, const ubigint& that) { 
-   return out << "ubigint(" << that.ubig_value[0] << ")";
+  return out << "ubigint(" << that.ubig_value[0] << ")";
 }
 
