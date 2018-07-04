a3f0d0556dcc89f75b0ae271324e349885282f36
Connor Thurmond <cthurmon@ucsc.edu>
2018-07-04 12:44:55 -0700

    Changed tabs to spaces.

--- code/main.cpp
+++ code/main.cpp
@@ -131,7 +131,7 @@ int main (int argc, char** argv) {
                   throw ydc_quit();
                   break;
                case tsymbol::NUMBER:
-               	  cout << "We got this far.";
+                  cout << "We got this far.";
                   operand_stack.push (bigint (lexeme.lexinfo));
                   break;
                case tsymbol::OPERATOR: {
