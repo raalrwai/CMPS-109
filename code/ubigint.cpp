b6610de411cccc4fd4dae81424407f652b01f6dd
Connor Thurmond <cthurmon@ucsc.edu>
2018-07-05 14:04:45 -0700

    Working on addition.

--- code/ubigint.cpp
+++ code/ubigint.cpp
@@ -66,7 +66,7 @@ ubigint ubigint::operator+ (const ubigint& that) const {
         }
         partial_result = digit1 + digit2;
         cout << digit1 << " + " << digit2 << " = " << partial_result << endl;
-        cout << "Result: " << result.ubig_value;
+        cout << "Result: " << result;
         result.ubig_value.push_back(partial_result);
         cout << "Result: " << result;
     }
