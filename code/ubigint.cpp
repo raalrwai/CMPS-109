6843910c0518d5f67c1b47b5411871059d0b2e1e
Connor Thurmond <cthurmon@ucsc.edu>
2018-07-05 12:08:18 -0700

    Changing types.

--- code/ubigint.cpp
+++ code/ubigint.cpp
@@ -39,7 +39,7 @@ ubigint ubigint::operator+ (const ubigint& that) const {
     //int carry = 0;
     cout << "that.ubig_value: " << that.ubig_value << endl;
     //cout << "ubig_value: " << ubig_value << endl;
-    for (auto it = that.ubig_value.rend(); it != that.ubig_value.rbegin(); it++) {
+    for (auto it = that.ubig_value.rbegin(); it != that.ubig_value.rend(); it++) {
         cout << "Digit: " << *it;
     }
 
