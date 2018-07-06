63fc97fc964a5013c4a5f946d0795a36f6ea761d
Connor Thurmond <cthurmon@ucsc.edu>
2018-07-06 10:36:11 -0700

    Working on subtraction.

--- code/ubigint.cpp
+++ code/ubigint.cpp
@@ -75,18 +75,17 @@ ubigint ubigint::operator- (const ubigint& that) const {
         iter_size = that.ubig_value.size();
     } 
 
-  //   vector<unsigned char> test;
-  //   for (unsigned char i = 0; i < 10; i++) {
-  //       test.push_back(i);
-  //   }
-  //   for (auto it = test.rbegin(); it != that.rend(); it++){
-  //   ubig_value.push_back(*it);
-  //   DEBUGF('~', *it);
-  // }
+    vector<unsigned char> test;
+    // for (unsigned char i = 0; i < 10; i++) {
+    //     test.push_back(i);
+    // }
+    for (auto it = test.rbegin(); it != test.rend(); it++){
+        test.push_back(*it);
+    }
 
-  //   for (int i = 0; i < 10; i++) {
-  //       cout << "Ubig_value: " << test[i] << endl;
-  //   }
+    for (int i = 0; i < 10; i++) {
+        cout << "Ubig_value: " << test[i] << endl;
+    }
  
     int partial_result = 0;
     for (int i = 0; i < iter_size; i++) {
