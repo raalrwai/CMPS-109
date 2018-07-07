2d3bb5a3ac7410451e0c7606101edc69f578e044
Connor Thurmond <cthurmon@ucsc.edu>
2018-07-07 16:57:11 -0700

    Working on printing results nicely.

--- code/bigint.cpp
+++ code/bigint.cpp
@@ -92,7 +92,7 @@ ostream& operator<< (ostream& out, const bigint& that) {
    	//out << "," << that.uvalue << ")";
    	out << (that.is_negative ? "-" : "");
    	for (int i = 0; i < (that.uvalue.size() - 1); i++) {
-   		out << that.uvalue.ubigvalue[i];
+   		out << that.uvalue.ubig_value[i];
    	}
    	return out;
 }
