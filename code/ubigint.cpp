fc59a4710e43e1ea3f477c700ba3f3b82365a390
Connor Thurmond <cthurmon@ucsc.edu>
2018-07-07 19:07:14 -0700

    Working on making addition work with carry.

--- code/ubigint.cpp
+++ code/ubigint.cpp
@@ -64,7 +64,8 @@ ubigint ubigint::operator+ (const ubigint& that) const {
             //cout << "Carry Result: " << (char) (carry_result + '0') << endl;
             //corrected_result = (carry_result-'0') % 10;
             corrected_result = 18 % 10;
-            cout << "Corrected Result: " << (char) (corrected_result + '0') << endl;
+            //cout << "Corrected Result: " << (char) (corrected_result + '0') << endl;
+            cout << "Corrected Result: " << static_cast<char>(corrected_result + '0') << endl;
             result.ubig_value.push_back(corrected_result + '0');
             carry = 1;
         } else {
