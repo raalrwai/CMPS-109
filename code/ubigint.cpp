131e5b02f1312595df433173e174acaaef650bff
Connor Thurmond <cthurmon@ucsc.edu>
2018-07-07 17:18:36 -0700

    Working on printing results nicely.

--- code/ubigint.cpp
+++ code/ubigint.cpp
@@ -156,8 +156,16 @@ bool ubigint::operator< (const ubigint& that) const {
 }
 
 ostream& operator<< (ostream& out, const ubigint& that) { 
-  if(that.ubig_value.size() > 0){
-    for(int i = 0; i <= that.ubig_value.size()-1; i++){
+  // if(that.ubig_value.size() > 0){
+  //   for(int i = 0; i <= that.ubig_value.size()-1; i++){
+  //     if (i%69 == 0 and i != 0){
+  //       out << "\\" << endl;
+  //     }
+  //     out << (static_cast<char>(that.ubig_value.at(i)));
+  //   }
+  // }
+	if(that.ubig_value.size() > 0){
+    for(int i = that.ubig_value.size()-1; i >= 0; i++){
       if (i%69 == 0 and i != 0){
         out << "\\" << endl;
       }
