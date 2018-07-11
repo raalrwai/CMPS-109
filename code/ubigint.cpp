524ced1f0d6fbda445091c9b22b4b144995a1578
Connor Thurmond <cthurmon@ucsc.edu>
2018-07-11 11:37:13 -0700

    Working on multiplication.

--- code/ubigint.cpp
+++ code/ubigint.cpp
@@ -231,7 +231,7 @@ bool ubigint::operator< (const ubigint& that) const {
 ostream& operator<< (ostream& out, const ubigint& that) { 
     if(that.ubig_value.size() > 0){
         for(int i = that.ubig_value.size()-1; i >= 0; i--){
-            if ((that.ubig_value.size() - i)%70 == 0 /*and i != 0*/){ 
+            if ((that.ubig_value.size() - i)%69 == 0 /*and i != 0*/){ 
                 out << "\\" << endl;
             }
             out << (static_cast<char>(that.ubig_value.at(i)));
