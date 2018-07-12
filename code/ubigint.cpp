b57673e515f56f5e624fb4ccc8bef855b19254a4
Connor Thurmond <cthurmon@ucsc.edu>
2018-07-11 17:15:25 -0700

    Working on division.

--- code/ubigint.cpp
+++ code/ubigint.cpp
@@ -164,7 +164,7 @@ void ubigint::multiply_by_2() {
     for(int i = 0; i < size + 1; i++){
         product.ubig_value.push_back(static_cast<udigit_t>(0)+ '0');
     }
-    cout << "tks" << endl;
+    cout << "tkr" << endl;
     int partial_product = 0;
     int carry = 0;
     for (int i = 0; i < size; i++) {
